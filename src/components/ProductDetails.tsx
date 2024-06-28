e21d4a1a906e4a8cc992418d56d723ecbf39b8d7
Domoulin Mata, Gilberto <gilberto.domoulinmata@cencosud.cl>
2024-06-27 21:59:36 -0400

    Fix All

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,16 +1,27 @@
-import { useNavigate } from "react-router-dom"
+import { ActionFunctionArgs, Form, useNavigate, redirect, useFetcher } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from '../utils/index';
+import { deleteProduct } from "../services/ProductService";
 
 type ProductDetailsProps = {
     product: Product;
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
+export async function action({params}: ActionFunctionArgs) {
+    if ( params.id !== undefined ) {
+        await deleteProduct(+params.id);
+        return redirect('/');
+    }
+}
+
 export default function ProductDetails({ product }: ProductDetailsProps) {
+
+  const fetcher = useFetcher();
 
   const navigate = useNavigate();
 
-  const isAvailable = product.availability ? 'Disponible' : 'No Disponible';
+  const isAvailable = product.availability;
   
   return (
     <tr className="border-b ">
@@ -21,7 +32,25 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
             { formatCurrency(product.price) }
         </td>
         <td className="p-3 text-lg text-gray-800">
-            { isAvailable }
+            <fetcher.Form method="POST">
+                <button
+                    type="submit"
+                    name="id"
+                    value={product.id}
+                    className={`${isAvailable ? 'text-black' : 'text-red-600'} 
+                        rounded-lg 
+                        p-2 
+                        text-xs 
+                        uppercase 
+                        font-bold w-full
+                        border
+                        border-black-100
+                        hover:cursor-pointer
+                    `}
+                >
+                    { isAvailable ? 'Disponible' : 'No Disponible' }
+                </button>
+            </fetcher.Form>
         </td>
         <td className="p-3 text-lg text-gray-800">
             <div className="flex gap-2 items-center">
@@ -31,6 +60,32 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
                 >
                     Editar
                 </button>
+
+                <Form
+                    className="w-full"
+                    method="POST"
+                    action={`productos/${ product.id }/eliminar`}
+                    onSubmit={(e) => {
+                        if (!confirm('¿Estás seguro de eliminar este producto?')) {
+                            e.preventDefault();
+                        }
+                    }}
+                >
+                    <input 
+                        type="submit" 
+                        value="Eliminar"
+                        className="
+                            bg-red-600 
+                            text-white 
+                            rounded-lg 
+                            w-full p-2 
+                            uppercase 
+                            font-bold 
+                            text-xs 
+                            text-center
+                            cursor-pointer"
+                    />
+                </Form>
             </div>
         </td>
     </tr>
